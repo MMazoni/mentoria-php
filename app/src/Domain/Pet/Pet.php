1dc07566c6572ec89a9b709ae47c4d6f2ce0d5c4
Matheus Mazoni <mmazoni.andrade@gmail.com>
2021-12-05 22:54:10 -0300

    Create database

--- app/src/Domain/Pet/Pet.php
+++ app/src/Domain/Pet/Pet.php
@@ -3,14 +3,26 @@
 namespace App\Domain\Pet;
 
 use App\Domain\ContactNumber;
+use Doctrine\ORM\Mapping\{Entity, Id, Column, GeneratedValue};
 
+#[Entity(repositoryClass: PetRepository::class)]
 class Pet
 {
+    #[Id]
+    #[Column(type: "integer")]
+    #[GeneratedValue]
+    private int $id;
+
     public function __construct(
+        #[Column(type: "string")]
         private string $name,
+        #[Column(type: "integer")]
         private int $age,
+        #[Column(type: "string")]
         private Animal $animal,
+        #[Column(type: "string")]
         private Breed $breed,
+        #[Column(type: "string")]
         private ContactNumber $contactNumber
     )
     {}
