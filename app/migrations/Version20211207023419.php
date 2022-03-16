20b4892f6ecc21e457c2faf06f34019d4b5a011e
Matheus Mazoni <mmazoni.andrade@gmail.com>
2022-03-16 16:45:09 +0000

    Glob to exclude migrations directory

--- app/migrations/Version20211207023419.php
+++ app/migrations/Version20211207023419.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211207023419 extends AbstractMigration
+final class version20211207023419 extends AbstractMigration
 {
     public function getDescription(): string
     {
